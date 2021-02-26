2469021f2247e272d9c3b87a3ba4094ff1fef9be
hellonayeon <nayeonkwon9815@gmail.com>
2021-02-26 15:18:21 +0900

    기사 타이틀 및 내용 받기

---
+++ hello.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+driver = webdriver.Chrome('./chromedriver')
+driver.implicitly_wait(3)
+driver.get('https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=105&oid=022&aid=0003556253')
+
+title = driver.find_element_by_id('articleTitle')
+body = driver.find_element_by_id('articleBodyContents')
+print(title.text)
+print(body.text)
